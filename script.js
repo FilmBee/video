c6afb61a2faffd3aa32fe47b709236d3e633d619
FilmBee <92419399+FilmBee@users.noreply.github.com>
2023-07-29 01:22:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Wait for the DOM to load
 document.addEventListener("DOMContentLoaded", function () {
   const videoPlayer = videojs("video-player");
 
-  // Function to load a video URL
   function loadVideo(url) {
     const videoSource = {
       src: url,
@@ -13,11 +11,88 @@ document.addEventListener("DOMContentLoaded", function () {
     videoPlayer.load();
   }
 
+  // Function to load and display available audio tracks
+  function loadAudioTracks() {
+    const audioTracks = videoPlayer.audioTracks();
+    const optionsContainer = document.getElementById("audio-tracks");
+
+    optionsContainer.innerHTML = ""; // Clear existing options
+
+    for (let i = 0; i < audioTracks.length; i++) {
+      const track = audioTracks[i];
+      const option = document.createElement("option");
+      option.value = i;
+      option.textContent = track.label || `Audio ${i + 1}`;
+      optionsContainer.appendChild(option);
+    }
+  }
+
+  // Function to load and display available subtitle tracks
+  function loadSubtitleTracks() {
+    const subtitleTracks = videoPlayer.textTracks();
+    const optionsContainer = document.getElementById("subtitle-tracks");
+
+    optionsContainer.innerHTML = ""; // Clear existing options
+
+    for (let i = 0; i < subtitleTracks.length; i++) {
+      const track = subtitleTracks[i];
+      if (track.kind === "subtitles") {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = track.label || `Subtitle ${i + 1}`;
+        optionsContainer.appendChild(option);
+      }
+    }
+  }
+
+  // Function to handle audio track change
+  function handleAudioTrackChange() {
+    const selectedAudioIndex = parseInt(
+      document.getElementById("audio-tracks").value
+    );
+    const audioTracks = videoPlayer.audioTracks();
+    for (let i = 0; i < audioTracks.length; i++) {
+      audioTracks[i].enabled = i === selectedAudioIndex;
+    }
+  }
+
+  // Function to handle subtitle track change
+  function handleSubtitleTrackChange() {
+    const selectedSubtitleIndex = parseInt(
+      document.getElementById("subtitle-tracks").value
+    );
+    const subtitleTracks = videoPlayer.textTracks();
+    for (let i = 0; i < subtitleTracks.length; i++) {
+      const track = subtitleTracks[i];
+      if (track.kind === "subtitles") {
+        track.mode = i === selectedSubtitleIndex ? "showing" : "hidden";
+      }
+    }
+  }
+
+  // Wait for the video player to be ready before setting up tracks
+  videoPlayer.ready(function () {
+    loadAudioTracks();
+    loadSubtitleTracks();
+
+    // Handle audio track change event
+    document.getElementById("audio-tracks").addEventListener("change", function () {
+      handleAudioTrackChange();
+    });
+
+    // Handle subtitle track change event
+    document.getElementById("subtitle-tracks").addEventListener("change", function () {
+      handleSubtitleTrackChange();
+    });
+  });
+
   // Handle form submission to load the video
   const videoForm = document.getElementById("video-form");
   videoForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const videoURL = document.getElementById("video-url").value;
     loadVideo(videoURL);
+    loadAudioTracks();
+    loadSubtitleTracks();
   });
 });
